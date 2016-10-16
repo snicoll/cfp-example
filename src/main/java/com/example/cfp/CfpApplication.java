873406a9be079bb39d6bdd6b478c8cd532970f4d
Stephane Nicoll <snicoll@pivotal.io>
2016-10-16 10:55:14 +0300

    Migrate Controller to WebMvcTest
    Rather than starting our entire application as a web application and
    configuring `MockMvc` ourselves we use the new "test slicing" feature
    of Spring Boot 1.4
    
    When using the dedicated `WebMvcTest` annotation, we tell Spring Boot
    we want to test a controller and that it has to do several things for us
    such as auto-configuring `MockMvc`.
    
    Another new feature of Spring Boot 1.4 demoed in this commit is to use
    `MockBean` rather than managing the mock ourselves.
    
    Slicing will tune component scan to only enable very specific beans. Our
    custom security config isn't applied anymore and we're back with the
    default auto-config that doesn't work for us since `/submit` is public.
    We import our `SecurityConfig` to make sure security is applied for this
    test.
    
    Also we can remove our override of `@EnableTransactionManagement`. Spring
    Boot 1.4 set this value to `true` by default but even if we set it to
    `false`, the infrastructure behind `MockBean` takes care of that problem
    for us.

--- src/main/java/com/example/cfp/CfpApplication.java
+++ src/main/java/com/example/cfp/CfpApplication.java
@@ -3,11 +3,9 @@ package com.example.cfp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CfpProperties.class)
-@EnableTransactionManagement(proxyTargetClass = true)
 public class CfpApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/cfp/web/CfpControllerTest.java
+++ src/test/java/com/example/cfp/web/CfpControllerTest.java
@@ -1,52 +1,38 @@
 package com.example.cfp.web;
 
-import com.example.cfp.CfpApplication;
 import com.example.cfp.domain.Submission;
 import com.example.cfp.domain.Track;
+import com.example.cfp.security.SecurityConfig;
 import com.example.cfp.submission.SubmissionRequest;
 import com.example.cfp.submission.SubmissionService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { CfpApplication.class, CfpControllerTest.TestConfig.class })
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = CfpController.class)
+@Import(SecurityConfig.class)
 public class CfpControllerTest {
 
 	@Autowired
-	private WebApplicationContext context;
-
-	@Autowired
-	private SubmissionService submissionService;
-
 	private MockMvc mvc;
 
-	@Before
-	public void setUp() {
-		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-		reset(this.submissionService);
-	}
+	@MockBean
+	private SubmissionService submissionService;
 
 	@Test
 	public void submitTalk() throws Exception {
@@ -67,16 +53,6 @@ public class CfpControllerTest {
 				.andExpect(status().isFound())
 				.andExpect(header().string(HttpHeaders.LOCATION, "/submit?navSection=submit"));
 		verify(this.submissionService).create(request);
-	}
-
-	@Configuration
-	static class TestConfig {
-
-		@Bean
-		public SubmissionService submissionService() {
-			return mock(SubmissionService.class);
-		}
-
 	}
 
 }
